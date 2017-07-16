2bbd23af0d54fd212881b29e42eb5723ba407022
Patrick Kohan <patrick.kohan@justwatch.com>
2017-07-16 19:32:10 +0200

    prototype of pure Go decrypt

--- gpg/gpg.go
+++ gpg/gpg.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+
+	"golang.org/x/crypto/openpgp"
 )
 
 const (
@@ -188,12 +190,53 @@ func (g *GPG) Encrypt(path string, content []byte, recipients []string) error {
 
 // Decrypt will try to decrypt the given file
 func (g *GPG) Decrypt(path string) ([]byte, error) {
-	args := append(g.args, "--decrypt", path)
-	cmd := exec.Command(g.binary, args...)
-	if g.debug {
-		fmt.Printf("[DEBUG] gpg.Decrypt: %s %+v\n", cmd.Path, cmd.Args)
+	// init some vars
+	var entity *openpgp.Entity
+	var entityList openpgp.EntityList
+
+	// Open the private key file
+	keyringFileBuffer, err := os.Open(os.Getenv("HOME") + "/.gnupg/secring.gpg")
+	if err != nil {
+		return nil, err
 	}
-	return cmd.Output()
+	defer keyringFileBuffer.Close()
+	entityList, err = openpgp.ReadKeyRing(keyringFileBuffer)
+	if err != nil {
+		return nil, err
+	}
+	entity = entityList[0]
+
+	fmt.Println("enter passphrase for private key")
+	scanner := bufio.NewScanner(os.Stdin)
+	ok := scanner.Scan()
+	if !ok {
+		return nil, fmt.Errorf("scanning line failed")
+	}
+
+	// Get the passphrase and read the private key.
+	// Have not touched the encrypted string yet
+	passphraseByte := []byte(scanner.Text())
+	entity.PrivateKey.Decrypt(passphraseByte)
+	for _, subkey := range entity.Subkeys {
+		subkey.PrivateKey.Decrypt(passphraseByte)
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decrypt it with the contents of the private key
+	md, err := openpgp.ReadMessage(f, entityList, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := ioutil.ReadAll(md.UnverifiedBody)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
 }
 
 // ExportPublicKey will export the named public key to the location given
